42d0cedd79bd18f27da2a35bf6e685bcebaa5495
Aaron Cookson <cookson.aaron@gmail.com>
2020-02-22 14:54:46 -0700

    added social links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import FbIcon from '../images/icons/social/facebook.svg';
+import GithubIcon from '../images/icons/social/github.svg';
+import InstaIcon from '../images/icons/social/instagram.svg';
+import LinkedinIcon from '../images/icons/social/linkedin.svg';
+import TwitterIcon from '../images/icons/social/twitter.svg';
 
 const Sidebar = () => (
   <div className='sidebar'>
-    <img src="https://media-exp1.licdn.com/dms/image/C5603AQHiVUv_t5gdeQ/profile-displayphoto-shrink_200_200/0?e=1587600000&v=beta&t=qzWdRGTeELZb80BDjj7hbWI98H44BWXHgJpcU7LFdq4" alt='profile pic' />
+    <div>
+      <img className='profile' src="https://media-exp1.licdn.com/dms/image/C5603AQHiVUv_t5gdeQ/profile-displayphoto-shrink_200_200/0?e=1587600000&v=beta&t=qzWdRGTeELZb80BDjj7hbWI98H44BWXHgJpcU7LFdq4" alt='profile pic' />
+    </div>
     <a>Home</a>
     <a>About</a>
     <a>Hobbies</a>
@@ -10,7 +17,16 @@ const Sidebar = () => (
     <a>Projects</a>
     <a>Resume</a>
     <a>Contact Me</a>
-    <a>*social links*</a>
+    <div className='social-wrapper'>
+      <a>Follow Me @</a>
+      <div className='social'>
+        <a href="https://www.github.com/ACookson95" target="_blank"><img src={GithubIcon} alt="social"/></a>
+        <a href="https://www.linkedin.com/in/aaron-cookson/" target="_blank"><img src={LinkedinIcon} alt="social"/></a>
+        <a href="https://www.facebook.com/Cookson.Aaron" target="_blank"><img src={FbIcon} alt="social"/></a>
+        <a href="https://www.instagram.com/aaron_cookson/" target="_blank"><img src={InstaIcon} alt="social"/></a>
+        <a href="https://www.twitter.com/aaroncookson?lang=en" target="_blank"><img src={TwitterIcon} alt="social"/></a>
+      </div>
+    </div>
   </div>
 )
 
